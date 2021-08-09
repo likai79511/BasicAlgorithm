92001893106d28328382418d2529498bb010666c
likai79511 <pruenature@163.com>
2021-08-09 10:59:46 +0800

    1.增加方法：生成测试数据 2.增加时间复杂度计算的描述

--- app/src/main/java/com/example/algorithm/CommonUtils.java
+++ app/src/main/java/com/example/algorithm/CommonUtils.java
@@ -1,24 +1,60 @@
 package com.example.algorithm;
 
+import java.util.Random;
+
 public class CommonUtils {
+
+    public static Random mRandom = new Random();
 
     public static class Singleton {
         public final static CommonUtils instance = new CommonUtils();
     }
 
-    private CommonUtils(){}
+    private CommonUtils() {
+    }
 
     public <E> String toArrayStr(E[] array) {
         StringBuffer sb = new StringBuffer();
         sb.append("[");
         for (int i = 0; i < array.length; i++) {
-            if (i != array.length-1){
-                sb.append(i+",");
-            }else{
+            if (i != array.length - 1) {
+                sb.append(i + ",");
+            } else {
                 sb.append(i);
             }
         }
         sb.append("]");
         return sb.toString();
     }
+
+    /**
+     * 生成指定长度的随机数据
+     *
+     * @param size
+     * @param max
+     * @return
+     */
+    public static int[] generateRandomData(int size, int max) {
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = mRandom.nextInt(max);
+        }
+        return data;
+    }
+
+    /**
+     * 生成指定长度的有序数组
+     *
+     * @param size
+     * @param desc  是否为降序数组
+     * @return
+     */
+    public static int[] generateOrderData(int size, boolean desc) {
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = desc ? size - i - 1 : i;
+        }
+        return data;
+    }
+
 }
--- app/src/main/java/com/example/algorithm/day01/LinearSearch.java
+++ app/src/main/java/com/example/algorithm/day01/LinearSearch.java
@@ -1,7 +1,11 @@
 package com.example.algorithm.day01;
 
 /**
- * 线性查找法
+ * 线性查找法 【查找】
+ * 思路：从第一个元素开始遍历比较，直到找到目标元素后停止动作。
+ * 查询速度和目标所在index有关： 如果目标所在index=0,查询的时间复杂度为O(1)， 如果index为
+ * size-1， 则时间复杂度为O(n).
+ * 时间复杂度分析按照最差的情况计算（上界），所以时间复杂度为 O(n)
  */
 public class LinearSearch {
 
